d063f70172563d2b2fbf3420bb0f249f7129ac32
Andres Flores <andres06037@gmail.com>
2019-12-03 14:11:16 -0600

    finished

--- day2.java
+++ day2.java
@@ -17,12 +17,33 @@ public class day2 {
         for (String num : line) {
             arrayOfInput.add(Integer.parseInt(num));
         }
-        
-        
 
-        // boolean keepGoing = true;
-        // while (keepGoing) {
+        System.out.println(arrayOfInput.toString());
 
-        // }
+        int a = 0, b = 0, outPutIndex = 0;
+
+        for (int i = 0; i < arrayOfInput.size() - 1; i++) {
+            if (arrayOfInput.get(i) == 99) {
+                break;
+            }
+            else if (arrayOfInput.get(i) == 1) {
+                a = arrayOfInput.get(arrayOfInput.get(i+1));
+                b = arrayOfInput.get(arrayOfInput.get(i+2));
+                outPutIndex = arrayOfInput.get(i+3);
+
+                arrayOfInput.set(outPutIndex,(a+b));
+                i+=3;
+            }
+            else if (arrayOfInput.get(i) == 2) {
+                a = arrayOfInput.get(arrayOfInput.get(i+1));
+                b = arrayOfInput.get(arrayOfInput.get(i+2));
+                outPutIndex = arrayOfInput.get(i+3);
+
+                arrayOfInput.set(outPutIndex,(a*b));
+                i+=3;
+            } 
+        }
+        System.out.println();
+        System.out.println(arrayOfInput.toString());
     }
 }
